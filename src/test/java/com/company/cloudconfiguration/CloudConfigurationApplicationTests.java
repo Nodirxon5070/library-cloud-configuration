90e21dce32cc73dcbbc5f02a292aa8a25dcc0e22
Nodirxon <nodirxon5070@gmail.com>
2023-11-07 17:13:02 +0500

    Git init

---
+++ src/test/java/com/company/cloudconfiguration/CloudConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.cloudconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
